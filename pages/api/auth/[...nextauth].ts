192adf0542587d0a2e1fad7fa51468f270ba96c4
pedro-olavo <polavo00@gmail.com>
2023-01-12 23:08:20 -0300

    feat: add refresh token function

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,51 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextAuthOptions } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import NextAuth from 'next-auth/next';
 import SpotifyProvider from 'next-auth/providers/spotify';
+
+async function refreshAccessToken(token: JWT) {
+  console.log('Entrou na função refresh token');
+  try {
+    const url =
+      'https://accounts.spotify.com/api/token' +
+      new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken,
+      });
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization:
+          'Basic ' +
+          new Buffer(
+            process.env.SPOTIFY_CLIENT_ID +
+              ':' +
+              process.env.SPOTIFY_CLIENT_SECRET,
+          ).toString('base64'),
+      },
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
 
 export const options: NextAuthOptions = {
   pages: {
@@ -9,15 +53,24 @@ export const options: NextAuthOptions = {
   },
 
   callbacks: {
-    jwt: async ({ token, account }) => {
-      if (account) {
-        token.accessToken = account.access_token;
+    jwt: async ({ token, account, user }) => {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+        };
       }
 
-      return token;
+      if (new Date() < new Date(token.exp * 1000)) {
+        return token;
+      }
+
+      return refreshAccessToken(token);
     },
+
     session: async ({ session, user, token }) => {
-      session.user.access_token = token.accessToken || 'no_token';
+      session.user.access_token = token.accessToken;
 
       return session;
     },
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -17,7 +17,11 @@ declare module 'next-auth/jwt' {
   /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT {
     /** OpenID ID Token */
-    accessToken?: string;
+    iat: number;
+    exp: number;
+    jti: string;
+    accessToken: string;
+    refreshToken: string;
   }
 }
 
