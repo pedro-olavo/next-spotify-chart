c77a4735bbdd1bbb8ad3b71f57cf7392e5586091
pedro-olavo <polavo00@gmail.com>
2023-03-08 20:48:27 -0300

    feat: adds current user api route

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,53 @@
 import React from 'react';
 import Image from 'next/image';
-import { redirect } from 'next/navigation';
-import { unstable_getServerSession } from 'next-auth';
 import { FiUser } from 'react-icons/fi';
+import { ArtistDisplay, Card, Button } from './components';
+import useAuth from './hooks/useAuth';
 
-import { options } from '../pages/api/auth/[...nextauth]';
-import SignOutButton from './SignOutButton.component';
-import ArtistDisplay from './components/ArtistDisplay';
+import artistSinging from '../public/artist-singing.jpg';
+import vinylRecord from '../public/vinyl-record.jpg';
+import { Artist, UserInfo } from '../types/userChartTypes';
 
-import { Artist, LastPlayedTracks } from '../types/userChartTypes';
-
-async function getSession() {
-  const session = await unstable_getServerSession(options);
-
-  if (!session) {
-    redirect('auth/signin');
-  }
-
-  return session;
-}
-
-async function getRecentlyPlayedArtists(token: string) {
-  const response = await fetch(
-    `https://api.spotify.com/v1/me/player/recently-played`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
+const getRecentlyPlayed = async (token: string) => {
+  const res = await fetch('http://localhost:3000/api/spotify/recently-played', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
     },
-  );
+  });
 
-  const data = await response.json();
+  const data = await res.json();
 
-  const lastPlayedArtists: Artist[] = data.items.reduce(
-    (unique: Artist[], item: LastPlayedTracks) => {
-      if (
-        !unique.some((artist) => artist.name === item.track.artists[0].name)
-      ) {
-        unique.push({
-          ...item.track.artists[0],
-          images: item.track.album.images,
-        });
-      }
+  return data as Artist[];
+};
 
-      return unique;
+const getCurrentUser = async (token: string) => {
+  const res = await fetch('http://localhost:3000/api/spotify/user', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
     },
-    [],
-  );
+  });
+  const data = await res.json();
 
-  return lastPlayedArtists;
-}
+  return data as UserInfo;
+};
 
 export default async function Home() {
-  const { user } = await getSession();
+  const user = await useAuth();
 
-  const recentlyPlayedArtists = await getRecentlyPlayedArtists(
-    user.access_token,
-  );
+  const recentlyPlayedArtists = await getRecentlyPlayed(user.access_token);
+  const userInfo = await getCurrentUser(user.access_token);
 
   return (
-    <div className="mx-auto flex min-h-screen w-full max-w-screen-lg flex-col items-center space-y-5 bg-spotify-black py-4 text-white">
-      <div className="flex w-full items-center justify-between px-4">
+    <div className="mx-auto flex min-h-screen w-full flex-col items-center space-y-5 bg-spotify-black py-5 text-white 2xl:space-y-8 ">
+      <div className="flex w-full items-center justify-between px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14 ">
         <div className="flex items-center space-x-3">
-          <button
-            className="relative flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full bg-stone-800"
-            type="button"
+          <a
+            className="relative flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full bg-stone-800 xl:w-28"
+            target="_blank"
+            href={userInfo.external_urls.spotify}
+            rel="noopener noreferrer"
           >
             {!user.image ? (
               <div className="text-4xl">
@@ -74,24 +56,41 @@ export default async function Home() {
             ) : (
               <Image src={user.image} alt="Profile Picture" fill />
             )}
-          </button>
-          <span className="text-2xl font-bold">Pedro</span>
+          </a>
+          <span className="text-2xl font-bold xl:text-4xl">
+            {user.name?.split(' ')[0]}
+          </span>
         </div>
 
-        <SignOutButton />
+        <Button.SignOut />
       </div>
 
-      <div className="flex w-full items-center justify-between px-4">
-        <span className="font-medium">Ouvidos Recentemente</span>
-        <span className=" font-bold text-spotify-green">Ver Mais</span>
+      <div className="flex w-full items-center justify-between px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+        <span className="text-lg font-semibold md:text-xl">
+          Ouvidos Recentemente
+        </span>
+        <span className="font-bold text-spotify-green md:text-lg">
+          Ver Mais
+        </span>
       </div>
 
-      <div className="w-full">
-        <div className="flex snap-x overflow-x-auto pb-3 scrollbar-hide lg:snap-none lg:scrollbar-default">
+      <div className="w-full pl-4 sm:pl-6 md:pl-8 lg:pl-10 xl:pl-12 2xl:pl-14">
+        <div className="flex snap-x space-x-3 overflow-x-auto pr-3 pb-4 scrollbar-hide lg:snap-none lg:scrollbar-default">
           {recentlyPlayedArtists?.map((artist, idx) => (
-            <ArtistDisplay {...artist} key={idx} />
+            <ArtistDisplay key={idx} {...artist} />
           ))}
         </div>
+      </div>
+
+      <div className="flex w-full items-center justify-between px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-14">
+        <span className="text-lg font-semibold md:text-xl">
+          Confira suas charts!
+        </span>
+      </div>
+
+      <div className="flex w-full flex-col space-y-5 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:flex-row 2xl:space-x-5 2xl:space-y-0 2xl:px-14">
+        <Card title="ARTISTAS" image={artistSinging} />
+        <Card title="MÚSICAS" image={vinylRecord} />
       </div>
     </div>
   );
